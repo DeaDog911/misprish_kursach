c6ea86d2bc9478d385ea22c62404c6dda1dde083
Даниил Н <daniil.nay03@gmail.com>
2024-05-26 18:43:36 +0300

    1.5 russian translation to the buttons

--- code/controller.py
+++ code/controller.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QApplication, QScrollArea, QGridLayout, QMainWindow
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QApplication, QScrollArea, QGridLayout, QMainWindow, QTableView
+from PyQt5.QtCore import Qt
 from code.model import Database
 from code.view import TableView
 
@@ -75,7 +76,8 @@ class Controller:
         table_names = self.get_table_names()
         row, col = 0, 0
         for name in table_names:
-            button = QPushButton(name[0], self.central_widget)
+            russian_name = self.get_russian_table_name(name[0])
+            button = QPushButton(russian_name, self.central_widget)
             button.clicked.connect(lambda _, table=name[0]: self.show_table_data(table))
             self.style_button(button)  # Применяем стиль к кнопке
             self.buttons.append(button)
@@ -84,6 +86,7 @@ class Controller:
             if col > 2:  # Меняем расположение кнопок в три столбца
                 col = 0
                 row += 1
+
 
     def style_button(self, button):
         button.setStyleSheet("""
@@ -115,8 +118,13 @@ class Controller:
     def show_table_data(self, table_name):
         sql_query = f"SELECT * FROM {table_name}"
         result = self.db.execute_query(sql_query)
-        print(f"данные из таблицы {table_name}:", *result, sep="\n")
-        self.view.update_data(result)
+        if result:
+            column_names = [description[0] for description in self.db.cur.description]
+            print(f"Названия столбцов:", column_names)
+            print(f"Данные из таблицы {table_name}:", *result, sep="\n")
+            self.view.update_data(result, column_names)
+        else:
+            print("Таблица пуста")
 
     def update_view(self):
         # По умолчанию показываем данные из первой таблицы
@@ -124,6 +132,28 @@ class Controller:
         if table_names:
             table_name = table_names[0][0]
             self.show_table_data(table_name)
+
+    @staticmethod
+    def get_russian_table_name(english_name):
+        match english_name:
+            case "classification":
+                return "классификация"
+            case "product":
+                return "продукт"
+            case "parameter":
+                return "параметры"
+            case "unit":
+                return "единицы измерения"
+            case "par_class":
+                return "классификация параметров"
+            case "par_prod":
+                return "продуктовые параметры"
+            case "pos_agr":
+                return "агрегация позиций"
+            case "pos_enum":
+                return "позиционные перечисления"
+            case _:
+                return english_name  # Если название не найдено, возвращаем английское название
 
 
 if __name__ == "__main__":
--- code/view.py
+++ code/view.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QTableWidgetItem, QTableWidget
 from PyQt5.QtGui import QFont
 
 class TableView(QWidget):
@@ -8,10 +8,26 @@ class TableView(QWidget):
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
-        self.label = QLabel()
-        self.label.setStyleSheet("QLabel { font-family: Arial; font-size: 12pt; }")  # Настройка шрифта
-        self.layout.addWidget(self.label)
+        self.table_widget = QTableWidget()
+        self.table_widget.setStyleSheet("QTableWidget { font-family: Arial; font-size: 12pt; }")  # Настройка шрифта
+        self.layout.addWidget(self.table_widget)
 
-    def update_data(self, new_data):
-        formatted_data = "<br>".join(str(row) for row in new_data)  # Форматированный вывод в столбец
-        self.label.setText(f"<html>{formatted_data}</html>")
+    def update_data(self, data, column_names=None):
+        if not data:
+            return
+
+        num_rows = len(data)
+        num_cols = len(data[0])
+
+        self.table_widget.setRowCount(num_rows)
+        self.table_widget.setColumnCount(num_cols)
+
+        # Установка заголовков столбцов
+        if column_names:
+            self.table_widget.setHorizontalHeaderLabels(column_names)
+
+        # Вывод данных в таблицу
+        for row_idx, row_data in enumerate(data):
+            for col_idx, cell_data in enumerate(row_data):
+                item = QTableWidgetItem(str(cell_data))
+                self.table_widget.setItem(row_idx, col_idx, item)
